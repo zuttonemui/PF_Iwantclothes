71f8414266e8916828f6c0ca5e2dcfb3631d7a12
zuttonemui <130944391+zuttonemui@users.noreply.github.com>
2023-07-26 06:50:06 +0000

    [update]layout

--- app/javascript/packs/typing.js
+++ app/javascript/packs/typing.js
@@ -8,30 +8,22 @@ $(document).on('turbolinks:load', function() {
         var windowHeight = $(window).height();
         var thisChild = "";
         if (scroll >= elemPos - windowHeight) {
-          thisChild = $(this).children(); //spanタグを取得
-          //spanタグの要素の１つ１つ処理を追加
+          thisChild = $(this).children();
           thisChild.each(function (i) {
             var time = 100;
-            //時差で表示する為にdelayを指定しその時間後にfadeInで表示させる
             $(this).delay(time * i).fadeIn(time);
           });
         } else {
           thisChild = $(this).children();
           thisChild.each(function () {
-            $(this).stop(); //delay処理を止める
-            $(this).css("display", "none"); //spanタグ非表示
+            $(this).stop();
+            $(this).css("display", "none");
           });
         }
       });
     }
-    // 画面をスクロールをしたら動かしたい場合の記述
-    $(window).scroll(function () {
-      TextTypingAnime();/* アニメーション用の関数を呼ぶ*/
-    });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
-    // 画面が読み込まれたらすぐに動かしたい場合の記述
     $(window).on('load', function () {
-      //spanタグを追加する
       var element = $(".TextTyping");
       element.each(function () {
         var text = $(this).html();
@@ -47,7 +39,7 @@ $(document).on('turbolinks:load', function() {
 
       });
 
-      TextTypingAnime();/* アニメーション用の関数を呼ぶ*/
-    });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+      TextTypingAnime();
+    });
   });
 });
